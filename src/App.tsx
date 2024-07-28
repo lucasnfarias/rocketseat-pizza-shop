e4ce85e0f673cb5c0ea9fa04bad1375d8c089190
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-28 16:38:45 -0300

    add and config react-router-dom dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './global.css'
 
-import { Button } from '@/components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from '@/routes'
 
 export function App() {
-  return (
-    <>
-      <div className="flex h-screen flex-col items-center justify-center gap-2">
-        <h2>Pizza shop</h2>
-        <Button>Teste</Button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
