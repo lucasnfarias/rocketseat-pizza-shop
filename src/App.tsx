f72b1b6c7b3d212ef6db9ec3ca18ac713f813cd7
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-28 16:13:47 -0300

    add shadcn/ui dep and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./global.css";
+
 export function App() {
   return (
     <>
-      <h2>Pizza shop</h2>
+      <div className="flex flex-col gap-2 justify-center items-center h-screen">
+        <h2>Pizza shop</h2>
+        <Button>Teste</Button>
+      </div>
     </>
   );
 }
