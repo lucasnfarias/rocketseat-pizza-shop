45b53f02e4102cf49db876018c96f53589b365e1
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-28 21:28:56 -0300

    add sonner toast dep

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -25,6 +26,13 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     await new Promise((resolve) => setTimeout(resolve, 2000))
     console.log(data)
+
+    toast.success('Enviamos um link de autenticação para seu e-mail.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
   }
 
   return (
