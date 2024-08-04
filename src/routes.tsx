084bc05a7d644878f6b62bcd1e3e226e255bc1dc
Lucas Farias <lucas.farias@rtbhouse.com>
2024-08-04 15:47:47 -0300

    create 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable simple-import-sort/imports */
 import { createBrowserRouter } from 'react-router-dom'
 
+import { NotFound } from '@/pages/404'
 import { AppLayout } from '@/pages/_layouts/app'
 import { AuthLayout } from '@/pages/_layouts/auth'
 import { Dashboard } from '@/pages/app/dashboard/dashboard'
@@ -11,6 +13,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
