120a6d683032c22ddb9c53e8fe8acee5652e70ce
Lucas Farias <lucas.farias@rtbhouse.com>
2024-08-12 20:30:59 -0300

    setup vitest and happy-dom deps

---
+++ src/pages/app/orders/order-status.spec.tsx
@@ -0,0 +1,3 @@
+test('1 + 1 equal 2', () => {
+  expect(1 + 1).toEqual(2)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
+})
