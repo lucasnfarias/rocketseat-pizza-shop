815fcce7c860e5b8e8c5fb42b9bc85498cba1ab7
Lucas Farias <lucas.farias@rtbhouse.com>
2024-08-14 20:20:25 -0300

    create sign-in tests

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,31 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+import { SignIn } from '@/pages/auth/sign-in'
+
+describe('Sign-In', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=johndoe@example.com']}
+            >
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@example.com')
+  })
+})
